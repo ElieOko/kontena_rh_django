8c63101d089057c91407f3ece2a11d90d41d5678
elieoko <elieoko100@gmail.com>
2024-05-18 12:46:39 +0100

    build project

---
+++ relation_sociale/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RelationSocialeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relation_sociale'
---
+++ temps_travail/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TempsTravailConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'temps_travail'
